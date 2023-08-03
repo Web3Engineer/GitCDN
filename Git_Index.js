fb60891c5464da6ccc6808cffd8268f61075eec9
web3engineer <web3engineer@gmail.com>
2023-08-02 21:33:42 -0400

    COMMENT->I Love Josephine

--- Git_Index.js
+++ Git_Index.js
@@ -1,1 +1,9 @@
+const shell = require('shelljs');
 const hi = "I Love Josephine";
+function runTerminal (_shellScript){ shell.exec(_shellScript); }
+
+const fileName = "Git_Index";
+const extension = "js";
+const comment = "Main-Push";
+
+const gitPush =(_fileName, _extension, _comment)=>{runTerminal(`git add ${_fileName}.${_extension} && git commit -m "${_comment}" && git push origin master`)};
