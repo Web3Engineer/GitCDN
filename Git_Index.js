e30a041c102d6b960f4f7009c164625c4b89e156
web3engineer <web3engineer@gmail.com>
2023-08-02 21:43:48 -0400

    Main-Push2

--- Git_Index.js
+++ Git_Index.js
@@ -4,7 +4,7 @@ function runTerminal (_shellScript){ shell.exec(_shellScript); }
 
 const fileName = "Git_Index";
 const extension = "js";
-const comment = "Main-Push";
+const comment = "Main-Push2";
 
 const gitPush =(_fileName, _extension, _comment)=>{runTerminal(`git add ${_fileName}.${_extension} && git commit -m "${_comment}" && git push origin master`)};
 
